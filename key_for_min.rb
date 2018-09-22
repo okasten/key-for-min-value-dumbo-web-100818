d919811cb413f19dc7ef4a8eca8eb8e8c9f70c38
Olivia Kasten <odkasten@gmail.com>
2018-09-22 00:44:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,11 @@
 def key_for_min_value(name_hash)
   smallest_key = 0
   current_value = 0
+  current_key = ""
   name_hash.collect do |key, value|
-    if value < current_value
+    if current_value == 0
+      current_value = value
+    elsif value < current_value
+      current_value = value 
+      
 end
